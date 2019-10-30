edd9ec71b0ed734415ca524769d316552fcd71f9
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-10-30 14:35:39 -0400

    update

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -14,13 +14,19 @@ $(document).ready(function () {
 
     //*scroll on buttons *//
 
-    $(".js--scroll-to-plans").click(function () {
-        $("html, body").animate({ scrollTop: $(".js--section-plans").offset().top }, 1000);
-    });
+    //*///////////DEACTIVATED QUICK LINK TO GITHUB AND LINKEDIN INSTEAD///////////////
 
-    $(".js--scroll-to-start").click(function () {
-        $("html, body").animate({ scrollTop: $(".js--section-features").offset().top }, 1000);
-    });
+    // $(".js--scroll-to-plans").click(function () {
+    //     $("html, body").animate({ scrollTop: $(".js--section-plans").offset().top }, 1000);
+    // });
+
+    // $(".js--scroll-to-start").click(function () {
+    //     $("html, body").animate({ scrollTop: $(".js--section-features").offset().top }, 1000);
+    // });
+
+
+    //*///////////DEACTIVATED QUICK LINK TO GITHUB AND LINKEDIN INSTEAD///////////////
+
 
     // *navigation scroll*//
 
